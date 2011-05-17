cc10523be11fe5f11a8dece559c24c90c4325e4f
Friedmann <friedmann@friedmann.intern.iwelt.de>
2011-05-17 12:28:42 +0200

    v1

---
+++ AndTekAgent/AppController.h
@@ -0,0 +1,38 @@
+//
+//  AppController.h
+//  AndTekAgent
+//
+//  Created by Friedmann on 17.05.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AppController : NSObject {
+    
+    /* Outlets */
+    IBOutlet    NSMenu      *statusMenu;
+    // Settings
+    IBOutlet    NSTextField *macAdresse;
+    IBOutlet    NSTextField *serverAdresse;
+    IBOutlet    NSTextField *portAdresse;
+    IBOutlet    NSTextField *apiAdresse;
+    IBOutlet    NSWindow    *settingsWindow;
+    
+    /* Anderes */
+    NSStatusItem            *statusItem;
+    NSImage                 *statusOn;
+    NSImage                 *statusOff;
+    NSUserDefaults          *preferences;
+    
+}
+
+-(IBAction)doLogin:(id)sender;
+-(IBAction)doLogout:(id)sender;
+-(IBAction)showSettings:(id)sender;
+-(IBAction)saveSettings:(id)sender;
+-(IBAction)cancelSettings:(id)sender;
+-(IBAction)openurl:(id)sender;
+
+@end
---
+++ AndTekAgent/AppController.m
@@ -0,0 +1,150 @@
+//
+//  AppController.m
+//  AndTekAgent
+//
+//  Created by Friedmann on 17.05.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppController.h"
+
+static NSString * const kServerURL = @"http://192.168.100.238:8080/andphone/ACDService";
+
+@interface AppController()
+- (void) sendRequestWithState: (NSString *) state;
+- (void) login: (NSNotification *) notification;
+- (void) logoff: (NSNotification *) notification;
+@end
+
+@implementation AppController
+
+- (void) awakeFromNib{
+    
+    //Create the NSStatusBar and set its length
+    statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength] retain];
+    
+    //Used to detect where our files are
+    NSBundle *bundle = [NSBundle mainBundle];
+    
+    //Allocates and loads the images into the application which will be used for our NSStatusItem
+    statusOn = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"iconon" ofType:@"png"]];
+    statusOff = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"iconoff" ofType:@"png"]];
+    
+    //Sets the images in our NSStatusItem
+    [statusItem setImage:statusOff];
+    //[statusItem setAlternateImage:statusHighlightImage];
+    
+    //Tells the NSStatusItem what menu to load
+    [statusItem setMenu:statusMenu];
+    //Sets the tooptip for our item
+    [statusItem setToolTip:@"AndTekAgent"];
+    //Enables highlighting
+    [statusItem setHighlightMode:YES];
+    //UserDefaults holen
+    preferences = [NSUserDefaults standardUserDefaults];
+    //In TextField schreiben
+    [macAdresse setStringValue:[preferences stringForKey:@"mac"]];
+    [serverAdresse setStringValue:[preferences stringForKey:@"server"]];
+    [portAdresse setStringValue:[preferences stringForKey:@"port"]];
+    [apiAdresse setStringValue:[preferences stringForKey:@"api"]];
+}
+
+- (id) init
+{
+    
+    self = [super init];
+    
+    if (self)
+    {
+        [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver: self selector: @selector(login:) name:  NSWorkspaceSessionDidBecomeActiveNotification object: nil];
+        [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver: self selector: @selector(logoff:) name: NSWorkspaceSessionDidResignActiveNotification object: nil];
+    }
+    
+    return self;
+}
+
+- (void) sendRequestWithState: (NSString *) state
+{
+	NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL: [NSURL URLWithString: kServerURL]
+															  cachePolicy: NSURLRequestUseProtocolCachePolicy
+														  timeoutInterval: 20.0];
+	[theRequest setHTTPMethod: @"POST"];
+	const char *stringToSend = [[NSString stringWithFormat: @"queue=all&setsec=-1&page=available&state=%@&dev=SEP%@", state, [macAdresse stringValue]] UTF8String];
+	[theRequest setHTTPBody:[NSData dataWithBytes: stringToSend length: strlen(stringToSend)]];
+	
+	NSURLResponse *response;
+    [NSURLConnection sendSynchronousRequest: theRequest returningResponse: &response error:nil];
+}
+
+- (void) login: (NSNotification *) notification
+{
+    NSLog(@"Login: #%@ auf AndTek-Server %@", [macAdresse stringValue],[serverAdresse stringValue]);
+    [self sendRequestWithState: @"0"];
+    [statusItem setImage:statusOn];
+}
+
+- (void) logoff: (NSNotification *) notification
+{
+    NSLog(@"Logout: #%@ auf AndTek-Server %@", [macAdresse stringValue], [serverAdresse stringValue]);
+	[self sendRequestWithState: @"1"];
+    [statusItem setImage:statusOff];
+    
+}
+
+- (void) dealloc {
+    //Releases the 2 images we loaded into memory
+    [statusOn release];
+    [statusOff release];
+    [super dealloc];
+}
+
+-(IBAction)doLogin:(id)sender{
+    [self login: nil];
+}
+
+-(IBAction)doLogout:(id)sender{
+    [self logoff: nil];
+}
+
+-(IBAction)showSettings:(id)sender{
+    //UserDefaults holen
+    preferences = [NSUserDefaults standardUserDefaults];
+    //In TextField schreiben
+    [macAdresse setStringValue:[preferences stringForKey:@"mac"]];
+    [serverAdresse setStringValue:[preferences stringForKey:@"server"]];
+    [portAdresse setStringValue:[preferences stringForKey:@"port"]];
+    [apiAdresse setStringValue:[preferences stringForKey:@"api"]];
+    
+    [settingsWindow makeKeyAndOrderFront:self];
+    [NSApp activateIgnoringOtherApps:YES];
+    
+    
+}
+
+-(IBAction)saveSettings:(id)sender{
+    preferences = [NSUserDefaults standardUserDefaults];
+    [preferences setObject:[macAdresse stringValue] forKey:@"mac"];
+    [preferences setObject:[serverAdresse stringValue] forKey:@"server"];
+    [preferences setObject:[portAdresse stringValue] forKey:@"port"];
+    [preferences setObject:[apiAdresse stringValue] forKey:@"api"];
+    [preferences synchronize];
+    
+    [settingsWindow orderOut:nil];
+    
+    
+}
+
+-(IBAction)cancelSettings:(id)sender{
+    
+    [settingsWindow orderOut:nil];
+    
+    
+}
+
+-(IBAction)openurl:(id)sender{
+    
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://twitter.com/frdmn/"]];    
+    
+}
+
+@end
